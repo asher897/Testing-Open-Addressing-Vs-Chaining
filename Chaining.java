d61e405fe34471f75058dee4795935d0dd9a34c5
Aaron Sher <shraar001@myuct.ac.za>
2019-04-01 23:42:38 +0200

    Developed chaining class extended from HashTable class with appropriate emthods defined.

---
+++ Chaining.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Chaining extends HashTable{
+	private static final Exception NullPointerException = null;
+	private static final Exception DuplicateItemException = null;
+	private LinkedList<HashEntry>[] chainTable;
+
+
+	public Chaining(int tableSize) {
+		super(tableSize);
+		int i = 0;
+		this.tableSize = tableSize;
+		chainTable = new LinkedList[this.tableSize];
+		while((i < this.tableSize) && (chainTable[i] == null)) {
+			chainTable[i] = new LinkedList<HashEntry>();
+			i++;
+		}
+	}
+
+	public void insert(HashEntry data) throws Exception {
+		int hash = hashCode(data.getDate())%tableSize;
+		chainTable[hash].add(data);
+		currentSize++;
+		inserts++;
+
+	}
+
+	public int find(String date) throws Exception {
+		int hash = hashCode(date)%tableSize;
+		ListIterator searchList = chainTable[hash].listIterator(0);
+		probe++;
+		HashEntry current;
+		int index = 0;
+		while(searchList.hasNext()){
+			probe++;
+			current = (HashEntry)searchList.next();
+			if(current.getDate().equals(date)){
+				index = chainTable[hash].indexOf(current);
+				break;
+			}
+		}
+		return index;
+
+	}
+
+	public void printDateTime(String date) throws Exception{
+		HashEntry values = chainTable[hashCode(date)%this.tableSize].get(find(date));
+		System.out.println("The global value is "+values.getGlobal() +" and the voltage value is " +values.getVoltage()+".");
+	}
+
+
+
+}
